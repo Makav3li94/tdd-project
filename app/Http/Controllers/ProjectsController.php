c5cbaf5320d875536493cbdc15b79f3d1fe7724e
MAKAVELI <neogood@yahoo.com>
2020-10-08 14:07:53 +0330

    first commit_ tdd test project

---
+++ app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Project;
+use Illuminate\Http\Request;
+
+class ProjectsController extends Controller
+{
+	public function index(){
+		$projects = Project::all();
+
+		return view('projects.index',compact('projects'));
+	}
+
+	public function store(){
+		$attr = request()->validate([
+			'title'=>'required',
+			'description'=>'required'
+		]);
+		Project::create($attr);
+		return redirect('/projects');
+	}
+}
---
+++ tests/Feature/ProjectTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProjectTest extends TestCase {
+	use WithFaker, RefreshDatabase;
+
+	/** @test */
+
+	public function a_user_can_create_a_project() {
+
+		$this->withoutExceptionHandling();
+
+		$attr = [
+			'title'       => $this->faker->sentence,
+			'description' => $this->faker->paragraph
+		];
+		$this->post( '/projects', $attr )->assertRedirect( '/projects' );
+
+		$this->assertDatabaseHas( 'projects', $attr );
+
+		$this->get( '/projects' )->assertSee( $attr['title'] );
+	}
+
+	/** @test */
+	public function a_project_requires_a_title() {
+		$attr = factory('App\Project')->raw(['title'=>'']);
+		$this->post( '/projects', $attr )->assertSessionHasErrors( 'title' );
+	}
+	/** @test */
+	public function a_project_requires_a_description() {
+		$attr = factory('App\Project')->raw(['description'=>'']);
+		$this->post( '/projects', $attr )->assertSessionHasErrors( 'description' );
+	}
+}
